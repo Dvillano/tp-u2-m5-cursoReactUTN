52a5458b804fb2c4a5d06420da5aa3613e51cd8a
Dvillano <danie.villano@gmail.com>
2021-07-15 14:41:07 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import PrimerComponente from './Components/PrimerComponente'
 import './App.css';
 
-const vectorNombres = [
-  "Juan Carlos",
-  "Maria",
-  "Gerardo"
-];
+// const vectorNombres = [
+//   "Juan Carlos",
+//   "Maria",
+//   "Gerardo"
+// ];
+
+const vectorImagenes = [
+  { url: 'https://placeimg.com/80/80/people?id=1', title: 'Titulo 1', description:
+  'Descripcion imagen 1'},
+  { url: 'https://placeimg.com/80/80/people?id=2', title: 'Titulo 2', description:
+  'Descripcion imagen 2'},
+  { url: 'https://placeimg.com/80/80/people?id=3', title: 'Titulo 3', description:
+  'Descripcion imagen 3'},
+  { url: 'https://placeimg.com/80/80/people?id=4', title: 'Titulo 4', description:
+  'Descripcion imagen 4'}
+  ]
 
 function App() {
 
-  let respuesta = vectorNombres.map( el => 
-    <PrimerComponente name={el} />
-  );
+  // let respuesta = vectorNombres.map( el => 
+  //   <PrimerComponente name={el} />
+  // );
+
+  let respuesta = vectorImagenes.map((el,index) => 
+    <PrimerComponente url={el.url} title={el.title} description={el.description} key={index}/> 
+  )
 
   return (
     <div className="App">
--- src/Components/PrimerComponente.js
+++ src/Components/PrimerComponente.js
@@ -4,9 +4,13 @@ import React from 'react';
 
 export default function PrimerComponente(props) {
     return(
-        <div>
-
+        <div class="container">
+            <img src={props.url} alt=""></img> 
+            <div class="textContainer">
+                <h1>{props.title}</h1>
+                <h4>{props.description}</h4>
+            </div>
         </div>
-
+        
     );
 }
